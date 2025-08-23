1869e1ee2f2ef12d2f87f1e00cdbf1d82754efd2
Glowing Jellyfishing <glowingjellyfishing@outlook.com>
2025-08-23 07:01:35 +0000

    Fix

---
+++ data.js
@@ -0,0 +1,23 @@
+// data.js
+module.exports = [
+  {
+    title: "Node.js Documentation",
+    url: "https://nodejs.org/en/docs/",
+    description: "Official documentation for Node.js runtime."
+  },
+  {
+    title: "Express.js Guide",
+    url: "https://expressjs.com/",
+    description: "Fast, unopinionated, minimalist web framework for Node.js."
+  },
+    {
+    title: "GlowingTech",
+    url: "localhost:9991",
+    description: "GlowingTech's official website."
+  },
+  {
+    title: "MDN Web Docs",
+    url: "https://developer.mozilla.org/",
+    description: "Resources for developers, by developers."
+  }
+];
